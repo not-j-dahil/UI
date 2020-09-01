df55bca7c22da8ee216b4bb331d51ec781c31ac9
Jasper Dahil <dahilj@uni.coventry.ac.uk>
2020-09-01 20:00:53 +0100

    added function to button

--- simple_gui.py
+++ simple_gui.py
@@ -5,20 +5,27 @@ from tkinter import *
 #create the window
 root = Tk()
 
+#Variables
+counter = 0
+
 #modify root window
 root.title("Simple GUI")
 root.geometry("640x360")
 
-app = Frame(root)
-app.grid()
-
 #creating label
-label = Label(app, text = "This is a label!")
-label.grid()
+label = Label(root, text = "This is a label!")
+label.pack()
+
+def button1Funct():
+    '''Function for button1'''
+    global counter #global needed for 'UnboundLocalError: local variable 'counter' referenced before assignment'
+    counter += 1
+    print("rawr", counter)
 
 #creating a button
-button1 = Button(app, text = "This is a button")
-button1.grid()
+button1 = Button(root, text = "This is a button")
+button1["command"] = button1Funct
+button1.pack()
 
 #kick off the event loop
 root.mainloop()
