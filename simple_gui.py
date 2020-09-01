c9db3dab8f2b3e59ea12b472eed658dbda97806f
Jasper Dahil <dahilj@uni.coventry.ac.uk>
2020-09-01 19:39:32 +0100

    added a label to the UI

--- simple_gui.py
+++ simple_gui.py
@@ -3,12 +3,17 @@
 from tkinter import *
 
 #create the window
-
 root = Tk()
 
 #modify root window
 root.title("Simple GUI")
 root.geometry("640x360")
+
+app = Frame(root)
+app.grid()
+label = Label(app, text = "This is a label!")
+
+label.grid()
 
 #kick off the event loop
 root.mainloop()
