755399d6974ec48b882c683d31176776a53da513
Jasper Dahil <dahilj@uni.coventry.ac.uk>
2020-09-01 19:34:53 +0100

    created a GUI

--- simple_gui.py
+++ simple_gui.py
@@ -1,2 +1,14 @@
 #simple GUI
 
+from tkinter import *
+
+#create the window
+
+root = Tk()
+
+#modify root window
+root.title("Simple GUI")
+root.geometry("640x360")
+
+#kick off the event loop
+root.mainloop()
